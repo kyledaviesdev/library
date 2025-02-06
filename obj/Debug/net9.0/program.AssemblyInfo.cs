20019e6e8befea1cb275a19596ca20e81b93a526
kyledaviesdev <kylemdavies127@gmail.com>
2025-02-06 14:33:25 -0700

    commit

--- obj/Debug/net9.0/program.AssemblyInfo.cs
+++ obj/Debug/net9.0/program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f995f55d2091f71029376cee0e7e984b5b810d8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00cb2a76fd3ab76a44fbee8af0831524fb67538a")]
 [assembly: System.Reflection.AssemblyProductAttribute("program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
